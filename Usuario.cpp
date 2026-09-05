2405bdb8bb2a3760d97971b4e7a5b29fc2d04d6b
MEFA2406 <manuel.fernandez.alfaro@est.una.ac.cr>
2026-09-05 11:11:34 -0600

    Clase usuario

---
+++ Usuario.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sualf on 5/9/2026.
+//
+
+#include "Usuario.h"
---
+++ Usuario.h
@@ -0,0 +1,15 @@
+//
+// Created by sualf on 5/9/2026.
+//
+
+#ifndef PROYECTO1_USUARIO_H
+#define PROYECTO1_USUARIO_H
+
+
+class Usuario {
+private:
+
+};
+
+
+#endif //PROYECTO1_USUARIO_H
