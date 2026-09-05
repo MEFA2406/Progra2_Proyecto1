5f9c73759db4a8e56250550db0741f6b456d4d4f
MEFA2406 <manuel.fernandez.alfaro@est.una.ac.cr>
2026-09-05 15:41:06 -0600

    clase Usuario

--- Usuario.cpp
+++ Usuario.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "Usuario.h"
+
+Usuario::Usuario(string& nombre, string& id): nombre(nombre), id(id) {
+}
+
+Usuario::~Usuario() {
+}
+
+string Usuario::getNombre() const {
+    return nombre;
+}
+
+string Usuario::getId() const {
+    return id;
+}
--- Usuario.h
+++ Usuario.h
@@ -1,13 +1,26 @@
 //
-// Created by sualf on 5/9/2026.
+// Created by manol on 5/9/2026.
 //
 
 #ifndef PROYECTO1_USUARIO_H
 #define PROYECTO1_USUARIO_H
 
+#include <string>
+using namespace std;
 
 class Usuario {
 private:
+    string nombre;
+    string id;
+public:
+    Usuario(string& nombre, string& id);
+    virtual ~Usuario();
+
+    string getNombre() const;
+    string getId() const;
+
+    virtual string getTipo() const =0;
+    virtual string toString() const=0;
 
 };
 
