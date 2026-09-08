12da3a94d3f2deaad6b66dc9403800da7137aa19
meza <allison.meza@hotmail.com>
2026-09-08 15:41:11 -0600

    Menú

---
+++ Menu.h
@@ -0,0 +1,16 @@
+#ifndef PROYECTO1_MENU_H
+#define PROYECTO1_MENU_H
+#include "MaterialBiblioteca.h"
+#include <string>
+using namespace std;
+
+class Menu {
+    string registrarMaterial();
+    string listarMaterial();
+    string devolverMaterial();
+    string guardarMaterial();
+    string cargarMaterial();
+};
+
+
+#endif //PROYECTO1_MENU_H
